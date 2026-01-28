8d60e1c0c70048d511ff0ead0a90946a66c67650
KlevisKoloshi1 <kleviskoloshi@2rmlab.net>
2026-01-28 11:51:55 +0100

    Add application icon for the Electron app

--- frontend/main.js
+++ frontend/main.js
@@ -4,6 +4,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    // Set your custom app icon (relative to the project folder)
+    icon: "icon.ico",
   });
 
   win.loadURL("http://localhost:5173");
